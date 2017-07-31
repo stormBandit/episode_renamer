076b9cd4ce36f46acc764facc155c5b9890e330f
dalton <dalton.polhill@tulip.io>
2017-07-31 14:19:41 -0400

    working on scrubbing the titles properly and loading them into the new files

--- episode_renamer.py
+++ episode_renamer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 import os
 import re
@@ -21,12 +22,72 @@ def getNameFormat(pathToFile):
         try:
             re.compile(userRegex)
             regexAccepted = True
-            print "IS VALID"
         except re.error:
             regexAccepted = False          
-            print "IS NOT VALID"
+
+#this funciton is used to parse the inputted episode guide / file names text
+def parseEpisodeGuide(fileNames):
+    #read in the lines
+    episodes = open(fileNames, "r")
+    names = episodes.readlines()
+    namesDictionary = {};
 
 
+    #build the regex to check episode names
+    regexp = re.compile(r"[0-9.]+?[a-zA-Z0-9 ]+?")
+
+
+    #edit the lines to strip away all other text but the name of the episode
+    for name in names:
+        name.strip("\n")
+
+        if not name:
+            print "NULL"
+        
+        if regexp.search(name):
+            result = re.search("(?P<number>[0-9.]*) (?P<title>[a-zA-Z ',]*) *(?P<airDate>[0-9/]*)", name) 
+            for i in range(1,3):
+                if not result.group(i):
+                    print "ERROR: failed to read title name" 
+                    sys.exit()
+
+            #append the new key/value to the dict
+            namesDictionary[result.group("number")] = result.group("title").rstrip(" ")
+
+    return namesDictionary
+
+#this function searches the given directory for files matching the regex given and replaces the file names with the names given in the name file 
+def renameEpisodes(nameFormat, fileNames, directory):
+    pattern = re.compile(r"(?P<title>[a-zA-Z0-9 ]*).(?P<seasonEpisode>[a-zA-Z0-9 ]*).(?P<videoType>[0-9]+?p).(?P<junk>[a-zA-Z0-9 ]*).(?P<junk2>[a-zA-Z0-9 -]*).(?P<fileType>[a-zA-Z]*)")
+
+    #load the names of the files from the nameFile
+    episodeNames = parseEpisodeGuide(fileNames)
+
+    print episodeNames
+    raw_input("DONE")
+
+    #for each episode in the folder, rename it with the corresponding one from the naming file
+    for fileName in os.listdir(directory):
+        #search for the episode
+        result = pattern.search(fileName)
+        #check that the search resulted in a find
+        if result: 
+            for i in range(1,7):
+                if result.group(i):
+                    print str(i) +" = "+result.group(i) 
+            #rename the episode
+            season = re.search("s[0-9]{2}", result.group(""))
+
+            currentEpisode = re.search("s[0-9]{2}e[0-9][2]", result.group("seasonEpisode"))
+
+
+            print "curr: "+str(currentEpisode)
+
+
+        else:
+            print "The regex returned nothing!"
+
+        
 
 #check for proper inputs
 if (len(sys.argv) != 3) :
@@ -45,8 +106,10 @@ if os.listdir(sys.argv[2]) == []:
     sys.exit()
 
 #get the nameing format for the files 
-nameFormat = getNameFormat(sys.argv[2])
+nameFormat = "0"
+#nameFormat = getNameFormat(sys.argv[2])
 
-
+#search the directory and see if a match for the regex is found, if not report the error
+result = renameEpisodes(nameFormat, sys.argv[1], sys.argv[2])
 
 
