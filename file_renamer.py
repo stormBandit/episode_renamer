871abefdb93fdce630cf2c88acd9466486832051
stormBandit <dalton.polhill@gmail.com>
2017-12-13 11:11:53 -0500

    cleaning up
    making the script better and improving the userinput class

--- file_renamer.py
+++ file_renamer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import sys
-import os
-import re
 import functions
 from userInputClass import UserInput
+from parserClass import Parser
 
 # print out menu for user
 functions.print_main_menu()
@@ -12,21 +10,10 @@ functions.print_main_menu()
 # TODO this will be replaced by a file picker
 userInput = UserInput()
 userInput.nameFile = userInput.get_name_file()
-
 userInput.pathToFiles = userInput.get_path_to_files()
 
-# check the formatting for file names and path to the episodes
-#functions.check_name_file_formatting(userInput.nameFile)
-
-# get the nameing format for the files
-# nameFormat = "0"
-# nameFormat = getNameFormat(sys.argv[2])
-
-# search the directory and see if a match for the regex is found, if not report the error
-
-# result = renameEpisodes(nameFormat, sys.argv[1], sys.argv[2])
-# if result == 1:
-#    print "Success!"
-# else:
-#    print "Failure!"
-
+parser = Parser()
+parser.find_pattern(userInput.pathToFiles)
+# parser.find_delimiter(userInput.pathToFiles)
+if parser.ignore_list.__sizeof__() != 0:
+    print("ignoreList: " + str(parser.ignore_list))
--- userInputClass.py
+++ userInputClass.py
@@ -24,7 +24,7 @@ class UserInput:
 
         regexp = re.compile(r"[0-9]{1,2}[.][0-9]{1,2}[a-zA-Z0-9 ]+?")
 
-        for i in range(0, names.__sizeof__()):
+        for i in range(0, names.__len__()):
             line = names.__getitem__(i)
             result = re.search(regexp, line)
             if not result:
@@ -36,11 +36,10 @@ class UserInput:
 
     # TODO replace with a file picker eventually
     def get_name_file(self):
-
         while True:
-            print("Please enter the file that describes the names for the episodes that you want renamed")
+            print("\nPlease enter the file that describes the names for the episodes that you want renamed")
             inputted_value = input()
-            print("inputtedValue: " + inputted_value)
+
             if not os.path.isfile(inputted_value):
                 print("ERROR: The file does not exist. Please try again\n")
             else:
@@ -52,11 +51,10 @@ class UserInput:
 
     # TODO replace with a file picker eventually
     def get_path_to_files(self):
-
         while True:
-            print("Please input the directory containing the episodes you want renamed")
+            print("\nPlease input the directory containing the episodes you want renamed")
             inputted_value = input()
-            print("inputtedValue: " + inputted_value)
+
             if not os.path.exists(inputted_value):
                 print("ERROR: Path to files could not be found. Please try again\n")
             else:
@@ -64,5 +62,3 @@ class UserInput:
                     return inputted_value
                 else:
                     print("ERROR: Directory is empty, please try again\n")
-
-
